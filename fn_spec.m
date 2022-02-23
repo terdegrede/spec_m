a38c70745c510bd789b7173d010bb6f493e265e6
joaaaaaaaaao <joao.lizarraga@gmail.com>
2022-02-23 03:55:36 -0500

    added relevant files

--- fn_spec.m
+++ fn_spec.m
@@ -49,12 +49,11 @@ Pn = zeros(M, N);
 
 %% Main loop
 for t = 0: T
-    %disp(t(mod(t, 50)==0)) % Print generation
+    disp(t(mod(t, 50)==0)) % Print generation
     
     PB = P(:, end-B+1:end); % Set of mating segments  (full genome or mating trait) for comparison
-    % PB = P(:, 1:B);
+
     % Hamming distances of mating segment (full genome or mating trait)
-    % D = pdist2(PB, PB, 'hamming')*N; 
     D = pdist2(PB, PB, 'hamming')*B;
     
     m = 1;
@@ -64,13 +63,11 @@ for t = 0: T
         ix_al = randi([1, M]);
         al = P(ix_al, :);
         
-        % if isempty(find(D(ix_al, (1:ix_al)) <= G, 1)) == 0 % are there individuals close enough?
         if isempty(find(D(ix_al, :) <= G, 1)) == 0 % are there individuals close enough?
             % 2nd indiv
             vc = find(D(ix_al, :) <= G);
-            % bt = P(vc(randi([1, length(vc)])), :);
-            bt = P(min(vc), :);
-           
+            bt = P(vc(randi([1, length(vc)])), :);
+            
             % -
             tmp = round(rand(size(al)));
             rm = tmp.*al + (1-tmp).*bt;
@@ -78,7 +75,6 @@ for t = 0: T
                 % Offspring               
                 % Mutation
     
-
                 rm(n) = (rm(n)-(rand < mu))^2;
             end
             Pn(m, :) = rm;
@@ -91,5 +87,4 @@ for t = 0: T
     % Saving data: 
     save_parfor(M, N, B, t, mu, P)
     % Saving for each generation
-    t
 end
---
+++ save_parfor.m
@@ -0,0 +1,6 @@
+function save_parfor(M, N, B, T, mu, P)
+    
+save(['data_fn2/M_' num2str(M) '_N_' num2str(N) '_B_' num2str(B)...
+             '_T_' num2str(T) '_mu_' num2str(mu) '.mat']...
+            ,'P', 'T', 'mu')
+end
