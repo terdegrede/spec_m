b3e6828cd1366a3ebfa4b90608fc190707ddf714
terdegrede <skpr.tffof@gmail.com>
2022-05-23 09:21:49 -0300

    new figures

--- s_clust_data.m
+++ s_clust_data.m
@@ -9,16 +9,10 @@ N = 2500;           % Genes per individual
 % 
 B = 2200;            % Mating segment
 A = 300;
-dA = 4;
-%
+mu = 0.001;
+T = 200;
 
-mu = 0.001;         % Mutation rate
-T = 200;            % Numer of generations
-Trng = 1: T;
-
-parfor mm = 1: 100
-    clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
-end
+fig_avg_fn(M, N, B, A, mu, T);
 
 %% Case 2
 clc, clearvars
@@ -29,39 +23,32 @@ N = 2500;           % Genes per individual
 % 
 B = 1600;            % Mating segment
 A = 900;
-dA = 26;
-%
 
 mu = 0.001;         % Mutation rate
 T = 200;            % Numer of generations
-Trng = 1: T;
 
-parfor mm = 1: 100
-  
-    clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
-end
+fig_avg_fn(M, N, B, A, mu, T);
 
-
-%% Case 3
-clc, clearvars
-
-M = 500;            % Numer of individuals
-N = 2500;           % Genes per individual
-
-% 
-B = 1000;            % Mating segment
-A = 1500;
-dA = 60;
-%
-
-mu = 0.001;         % Mutation rate
-T = 200;            % Numer of generations
-Trng = 1: T;
-
-parfor mm = 1: 100
-    
-    clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
-end
+% % % % Case 3
+% % % clc, clearvars
+% % % 
+% % % M = 500;            % Numer of individuals
+% % % N = 2500;           % Genes per individual
+% % % 
+% % % 
+% % % B = 1000;            % Mating segment
+% % % A = 1500;
+% % % dA = 60;
+% % % 
+% % % 
+% % % mu = 0.001;         % Mutation rate
+% % % T = 200;            % Numer of generations
+% % % Trng = 1: T;
+% % % 
+% % % parfor mm = 1: 100
+% % %     
+% % %     clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
+% % % end
 
 %% Case 4
 clc, clearvars
@@ -84,6 +71,7 @@ parfor mm = 1: 100
     clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
 end
 
+fig_avg_fn(M, N, B, A, mu, T);
 
 %% Case 5
 clc, clearvars
@@ -105,6 +93,8 @@ parfor mm = 1: 100
     clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
 end
 
+fig_avg_fn(M, N, B, A, mu, T);
+
 %% Case 6
 clc, clearvars
 
@@ -125,6 +115,9 @@ parfor mm = 1: 100
     clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
 end
 
+fig_avg_fn(M, N, B, A, mu, T);
+
+
 %% Case 8
 clc, clearvars
 
@@ -144,11 +137,13 @@ Trng = 1: T;
 parfor mm = 1: 100
     clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
 end
+fig_avg_fn(M, N, B, A, mu, T);
 
-%% 
-% parfor mm = 1: 100
-    mm = 100; % To delete when uncommenting
-    Trng = T; % To delete when uncommenting
-    clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
-% end
-borrar % To delete when uncommenting
+
+% %% 
+% % parfor mm = 1: 100
+%     mm = 100; % To delete when uncommenting
+%     Trng = T; % To delete when uncommenting
+%     clust_data_fn(A, B, M, N, mu, Trng, mm, dA)
+% % end
+% borrar % To delete when uncommenting
