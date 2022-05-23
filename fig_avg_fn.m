1222a67f19b90f460d93b4091666630272bfbbde
terdegrede <skpr.tffof@gmail.com>
2022-05-23 09:58:41 -0300

    update to save 4 figures

--- fig_avg_fn.m
+++ fig_avg_fn.m
@@ -13,15 +13,55 @@ avg_N = mean(NOC_N, 2);
 avg_A = mean(NOC_A, 2);
 avg_B = mean(NOC_B, 2);
 
-%
 f1 = figure('Visible','off');
+scatter(1:200, NOC_N, 'k', 'filled', 'MarkerFaceAlpha',.005)
+hold on
+plot(avg_N, 'r', 'LineWidth', 4)
+axis([0 200 0 25])
+
+
+%
+f2 = figure('Visible','off');
+scatter(1:200, NOC_B, 'k', 'filled', 'MarkerFaceAlpha',.005)
+hold on
+plot(avg_N, 'b--', 'LineWidth', 4)
+axis([0 200 0 25])
+
+%
+f3 = figure('Visible','off');
+scatter(1:200, NOC_A, 'k', 'filled', 'MarkerFaceAlpha',.005)
+hold on
+plot(avg_N, 'g', 'LineWidth', 4)
+axis([0 200 0 25])
+
+%
+f4 = figure('Visible','off');
 plot(avg_N, 'r', 'LineWidth', 4)
 hold on
 plot(avg_B, 'b--', 'LineWidth', 4)
-plot(avg_A, 'k', 'LineWidth', 4)
+plot(avg_A, 'g', 'LineWidth', 4)
 legend('Full genome', 'B segment', 'A segment','FontSize', 24)
 axis([0 200 0 25])
 grid minor
 set(gca,'FontSize',24)
-saveas(f1, ['fig_M_' num2str(M) '_N_' num2str(N) '_A_' num2str(A)... 
-    '_B_' num2str(B) '_T_' num2str(T) '_mu_' num2str(mu)], 'fig')
+
+% Saving
+fig_name1 = strcat('fig_avgN_M_', num2str(M), '_N_', num2str(N), '_A_',...
+    num2str(A), '_B_', num2str(B), '_T_', num2str(T),...
+    '_mu_', num2str(mu));
+savefig(f1, [fig_name1 '.fig'])
+%
+fig_name2 = strcat('fig_avgB_M_', num2str(M), '_N_', num2str(N), '_A_',...
+    num2str(A), '_B_', num2str(B), '_T_', num2str(T),...
+    '_mu_', num2str(mu));
+savefig(f2, [fig_name2 '.fig'])
+%
+fig_name3 = strcat('fig_avgA_M_', num2str(M), '_N_', num2str(N), '_A_',...
+    num2str(A), '_B_', num2str(B), '_T_', num2str(T),...
+    '_mu_', num2str(mu));
+savefig(f3, [fig_name3 '.fig'])
+%
+fig_name4 = strcat('fig_avg3_M_', num2str(M), '_N_', num2str(N), '_A_',...
+    num2str(A), '_B_', num2str(B), '_T_', num2str(T),...
+    '_mu_', num2str(mu));
+savefig(f4, [fig_name4 '.fig'])
