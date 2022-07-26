990ec604de934df026c1d7774bdc2ebcf0faab74
terdegrede <skpr.tffof@gmail.com>
2022-07-26 09:47:23 -0300

    uncomment

--- scripts_3CDH/main_DataGen.m
+++ scripts_3CDH/main_DataGen.m
@@ -6,7 +6,7 @@ mu = 1e-3;%2.5e-3;          % Mutation rate
 T = 500;            % Number of generations
 mm = 1;             % Run index
 
-% P = asex_fn(M, F, mu, T, mm);
+P = asex_fn(M, F, mu, T, mm);
 
 %% Homogeneous population model
 clc, clearvars, close all
@@ -17,7 +17,7 @@ T = 500;            % Number of generations
 GF = F + 1;
 mm = 1;             % Run index
 
-% P = sex_fn(M, F, mu, T, GF, mm);
+P = sex_fn(M, F, mu, T, GF, mm);
 
 %% DH model
 clc, clearvars, close all
@@ -28,7 +28,7 @@ T = 500;            % Number of generations
 GF = 0.05*F;
 mm = 1;             % Run index
 
-% P = sex_fn(M, F, mu, T, GF, mm);
+P = sex_fn(M, F, mu, T, GF, mm);
 
 %% Assort   
 clc, clearvars, close all
@@ -41,7 +41,7 @@ T = 500;            % Number of generations
 GC = 0.05*C;
 mm = 1;             % Run index
 
-% P = assor_fn(M, F, C, A, mu, T, GC, mm);
+P = assor_fn(M, F, C, A, mu, T, GC, mm);
 %% Assort B
 clc, clearvars, close all
 M = 500;            % Number of individuals
@@ -53,7 +53,7 @@ T = 500;            % Number of generations
 GC = 0.05*C;
 mm = 1;             % Run index
 
-% P = assor_fn(M, F, C, A, mu, T, GC, mm);
+P = assor_fn(M, F, C, A, mu, T, GC, mm);
 
 
 
