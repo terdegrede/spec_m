088b5288f2925e6ba4aa6e1c89b11efd4c66e4c1
terdegrede <skpr.tffof@gmail.com>
2022-07-27 14:55:44 -0300

    to get data from several runs

--- scripts_3CDH/main_DataGen.m
+++ scripts_3CDH/main_DataGen.m
@@ -1,59 +1,72 @@
-%% Asexual model
+%% Asexual model (Fig01a)
 clc, clearvars, close all
 M = 500;            % Number of individuals
-F = 10000;            % Genes per individual
-mu = 1e-3;%2.5e-3;          % Mutation rate
+F = 10000;           % Genes per individual
+mu = 1e-3;          % Mutation rate
 T = 500;            % Number of generations
 mm = 1;             % Run index
 
 % P = asex_fn(M, F, mu, T, mm);
 
-%% Homogeneous population model
+%% Homogeneous population model (Fig01b)
 clc, clearvars, close all
 M = 500;            % Number of individuals
-F = 10000;            % Genes per individual
-mu = 1e-3;%2.5e-3;          % Mutation rate
+F = 10000;          % Genes per individual
+mu = 1e-3;          % Mutation rate
 T = 500;            % Number of generations
 GF = F + 1;
 mm = 1;             % Run index
 
 % P = sex_fn(M, F, mu, T, GF, mm);
 
-%% DH model
+%% DH model (Fig01c, Fig02a)
 clc, clearvars, close all
 M = 500;            % Number of individuals
-F = 10000;            % Genes per individual
-mu = 1e-3;%2.5e-3;          % Mutation rate
+F = 10000;          % Genes per individual
+mu = 1e-3;          % Mutation rate
 T = 500;            % Number of generations
 GF = 0.05*F;
 mm = 1;             % Run index
 
-P = sex_fn(M, F, mu, T, GF, mm);
+% P = sex_fn(M, F, mu, T, GF, mm);
 
-%% Assort   
-clc, clearvars, close all
-M = 500;            % Number of individuals
-F = 2500;            % Genes per individual
-C = 100;
-A = 100;
-mu = 1e-3;%2.5e-3;          % Mutation rate
-T = 500;            % Number of generations
-GC = 0.05*C;
-mm = 1;             % Run index
-
-P = assor_fn(M, F, C, A, mu, T, GC, mm);
-%% Assort B
+%% Assort (Fig02b)
 clc, clearvars, close all
 M = 500;            % Number of individuals
 F = 2500;            % Genes per individual
 C = 1500;
 A = 1000;
-mu = 1e-3;%2.5e-3;          % Mutation rate
+mu = 1e-3;          % Mutation rate
 T = 500;            % Number of generations
 GC = 0.05*C;
 mm = 1;             % Run index
 
-P = assor_fn(M, F, C, A, mu, T, GC, mm);
+% P = assor_fn(M, F, C, A, mu, T, GC, mm);
 
 
+%% Assort (Fig02c)
+clc, clearvars, close all
+M = 500;            % Number of individuals
+F = 2500;            % Genes per individual
+C = 100;
+A = 100;
+mu = 1e-3;          % Mutation rate
+T = 500;            % Number of generations
+GC = 0.05*C;
+mm = 1;             % Run index
 
+% P = assor_fn(M, F, C, A, mu, T, GC, mm);
+
+%% Assort (Fig 03)
+clc, clearvars, close all
+M = 500;            % Number of individuals
+F = 2500;            % Genes per individual
+C = 100;
+A = 100;
+mu = 1e-3;          % Mutation rate
+T = 500;            % Number of generations
+GC = 0.05*C;
+
+parfor mm = 2: 75
+    P = assor_fn(M, F, C, A, mu, T, GC, mm);   
+end
